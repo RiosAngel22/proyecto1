aac29c03185643b1a9d552f00de23fa7e571d2cf
RiosAngel22 <angelagustin1222@gmail.com>
2025-10-10 14:34:32 -0300

    quitado debug log innecesario

--- proyecto1/Assets/scripts/enemigo/GeneradorObjetoLoop.cs
+++ proyecto1/Assets/scripts/enemigo/GeneradorObjetoLoop.cs
@@ -19,13 +19,11 @@ public class GeneradorObjetoLoop : MonoBehaviour
 
     private void OnBecameInvisible()
     {
-        Debug.Log("El SpriteRenderer deja de ser visible por las camaras de la escena");
         CancelInvoke(nameof(GenerarObjeto));
     }
 
     private void OnBecameVisible()
     {
-        Debug.Log("El SpriteRenderer se vuelve visible para las camaras de la escena");
         InvokeRepeating(nameof(GenerarObjeto), tiempoEspera, tiempoIntervalo);
     }
 }
